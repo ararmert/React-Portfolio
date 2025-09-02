8f63ab0534b3cd6363f1c8991daaf425c897e5b7
Mert Arar <ararmert@outlook.com>
2025-09-02 17:48:58 +0200

    Init commit: React Portfolio App

---
+++ React/an-app/src/App.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+
+
+const user = {
+  name: 'Mert Arar',
+  imageUrl: 'https://i.imgur.com/6yHc5fX.jpeg',
+  imagesize: 300,
+
+};
+
+
+function AButton({ setPage, target, label}) {
+    function click() {
+      alert('See, I know how to make buttons :)')
+      setPage(target);
+      
+    }
+
+    return (
+        <button onClick={click}>
+          {label}
+        </button>
+    );
+}
+
+
+function Profile() {
+  return (
+    <>
+      <h1> Welcome to the interactive portfolio of Mert Arar </h1>
+      <img 
+        className = "avatar"
+        src={user.imageUrl}
+        
+        style={{
+          width: user.imagesize,
+          height: user.imagesize
+        }}
+        />
+        </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <h1>Technical Skills</h1>
+      <h2>Programming languages</h2>
+      <ul>
+        <li>C</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>Haskell</li>
+        <li>Currently learning JavaScript and React</li>
+      </ul>
+    
+    
+    </>
+  );
+}
+
+export default function App() { // main component in the JS file
+  const [page, setPage] = useState("home");
+    
+    return (
+        <div>
+           
+            
+            {page === "home" ? <Profile /> : <About />}
+            
+            {page === "home" 
+            ? <AButton setPage={setPage} target= "about" label={"About"} />
+            : <AButton setPage={setPage} target= "home" label={"Go Back"} />
+}
+            
+
+        </div>
+    );
+}
---
+++ React/an-app/src/App.jsx
@@ -0,0 +1,18 @@
+function AButton() {
+    return (
+        <button>
+          Details of my portfolio
+        </button>
+    );
+}
+
+export default function AnApp() { // main component in the JS file
+    return (
+        <div>
+            <h1>
+                Welcome to the interactive portfolio of Mert Arar
+            </h1>
+
+        </div>
+    );
+}
