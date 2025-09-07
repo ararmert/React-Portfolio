e7c32944b1454defe5dbff4cb8f8e44a0a3f7920
Mert Arar <ararmert@outlook.com>
2025-09-07 18:57:51 +0200

    Styling update

--- React/an-app/src/App.js
+++ React/an-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
 
 const user = {
   name: 'Mert Arar',
@@ -9,19 +10,46 @@ const user = {
 
 };
 
+const theme = {
+  blue: {
+    default: '#080C40',
+    hover: '#36364fff'
+  }
+}
+
 function AButton({ setPage, target, label }) {
+  const [hover, setHover] = React.useState(false);
   function click() {
     const key = `clicked:${target}`; // unique per target
-    if (!localStorage.getItem(key)) {
+    if (!localStorage.getItem(key) || !target === "minigames") {
       alert(`See, I know how to make a button :)`);
       localStorage.setItem(key, '1');
     }
     setPage(target);
   }
-  return <button onClick={click}>{label}</button>;
+  return (
+    <button 
+      onClick={click}
+      onMouseEnter={() => setHover(true)}   
+      onMouseLeave={() => setHover(false)}
+      style={{
+        backgroundColor: hover ? theme.blue.hover : theme.blue.default,
+        color: 'white',
+        border: '0',
+        padding: '10px 20px',
+        borderRadius: '6px',
+        cursor: 'pointer',
+        transition: 'background-color 0.2s'
+      }}
+    >
+      {label}
+    </button>
+  );
 }
 
 function BButton({ setPage, target, label }) {
+  
+  const [hover, setHover] = React.useState(false);
    
     function click() {
     const key = `clicked:${target}`; // unique per target
@@ -31,7 +59,24 @@ function BButton({ setPage, target, label }) {
     }
     setPage(target);
   }
-  return <button onClick={click}>{label}</button>;
+  return (
+    <button 
+      onClick={click}
+      onMouseEnter={() => setHover(true)}   
+      onMouseLeave={() => setHover(false)}
+      style={{
+        backgroundColor: hover ? theme.blue.hover : theme.blue.default,
+        color: 'white',
+        border: '0',
+        padding: '10px 10px',
+        borderRadius: '6px',
+        cursor: 'pointer',
+        transition: 'background-color 0.2s'
+      }}
+    >
+      {label}
+    </button>
+  );
 }
 
 function CButton({ setPage, target, label }) {
@@ -43,9 +88,10 @@ function CButton({ setPage, target, label }) {
 }
 
 function Profile() {
+  
   return (
     <>
-      <h1> Welcome to the interactive portfolio of 
+      <h1 id ="mainH" style={{ color: "darkblue" }}> Welcome to the interactive portfolio of 
         Mert Arar </h1>
       <img 
         className = "avatar"
@@ -77,6 +123,16 @@ function About() {
   );
 }
 
+
+function MiniGames() {
+  return (
+    <>
+      <h1>Currently under construction</h1>
+    </>
+  );
+}
+
+
 function UpComing() {
   const upcomingfeatures = [
     { title: 'Tic-Tac-Toe', isNext: true, id: 1},
@@ -87,6 +143,7 @@ function UpComing() {
 
   const done = [
     { title: 'Click counter to only display the annoying message on the first click', isNext: false, id: 4},
+    { title: 'Animations', isNext: false, id: 5},
   ];
 
   const listFeatures = upcomingfeatures.map(feature =>
@@ -135,6 +192,7 @@ export default function App() { // main component in the JS file
   const views = {
     home: <Profile />,
     about: <About />,
+    minigames: <MiniGames />,
     upcoming: <UpComing />
   };
     
@@ -157,15 +215,19 @@ export default function App() { // main component in the JS file
             {views[page]}
             
             {page === "home" ? ( // if on home, run the first block, show 2 buttons
-              <>
-              <br />
+              
+              
+              <div style={{ marginTop: '10px', display: 'flex', gap: '20px', justifyContent: 'normal' }}>
               <AButton setPage={setPage} target="about"    label="About Me"/>
-              &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-              <BButton setPage={setPage} target="upcoming" label="Upcoming Features"/>   
-              </>
+              <AButton setPage={setPage} target="minigames"   label="🎮"/>
+                
+              <BButton setPage={setPage} target="upcoming" label="Upcoming Features"/> 
+              </div>  
+              
             ) : ( // if not on home, only 1 button to go back home
                <>
-               <br />
+               
+               
                &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
               <CButton setPage={setPage} target="home" label="Go Back"/>
               </>
